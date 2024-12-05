952fc6118dc759cf50e028c1ad1e484c5bce98c6
Chauhan Vivek Singh <105410640+chauhanviveksingh@users.noreply.github.com>
2024-12-05 15:13:28 +0530

    first commit

---
+++ starSquare.java
@@ -0,0 +1,32 @@
+package pattern;
+
+import java.util.Scanner;
+
+public class starSquare {
+    public static void main(String [] args ){
+        Scanner sc= new Scanner(System.in);
+    System.out.print("Enter square size");
+    int n= sc.nextInt();
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=n; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
